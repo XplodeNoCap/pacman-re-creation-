795faedd1358466563f98fa94493fba84c5da7c9
David <111158241+XplodeNoCap@users.noreply.github.com>
2024-01-24 01:16:25 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function lives () {
+	
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+	
+})
 let Player1 = sprites.create(img`
     . . f f f f f f f f f . . 
     . f f 5 5 5 5 5 5 5 f f . 
@@ -33,7 +39,9 @@ let Ghosty_1 = sprites.create(img`
     . 8 8 8 . 8 8 8 . 8 8 8 . 8 8 8 
     . . 8 . . . 8 . . . 8 . . . 8 . 
     `, SpriteKind.Enemy)
-Ghosty_1.setPosition(randint(0, 10), randint(0, 10))
+Ghosty_1.setBounceOnWall(true)
+Ghosty_1.setPosition(200, 70)
+Ghosty_1.setVelocity(38, 40)
 let Ghosty2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . a a a a a . . . . . 
@@ -52,7 +60,9 @@ let Ghosty2 = sprites.create(img`
     . a a a . a a a . a a a . a a a 
     . . a . . . a . . . a . . . a . 
     `, SpriteKind.Enemy)
-Ghosty2.setPosition(randint(0, 10), randint(0, 10))
+Ghosty2.setBounceOnWall(true)
+Ghosty2.setVelocity(38, 40)
+Ghosty2.setPosition(280, 70)
 let Ghosty3 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 2 2 2 2 2 . . . . . 
@@ -71,7 +81,9 @@ let Ghosty3 = sprites.create(img`
     . 2 2 2 . 2 2 2 . 2 2 2 . 2 2 2 
     . . 2 . . . 2 . . . 2 . . . 2 . 
     `, SpriteKind.Enemy)
-Ghosty3.setPosition(randint(0, 10), randint(0, 10))
+Ghosty3.setBounceOnWall(true)
+Ghosty3.setPosition(200, 134)
+Ghosty3.setVelocity(38, 40)
 let Ghosty4 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 3 3 3 3 3 . . . . . 
@@ -90,6 +102,23 @@ let Ghosty4 = sprites.create(img`
     . 3 3 3 . 3 3 3 . 3 3 3 . 3 3 3 
     . . 3 . . . 3 . . . 3 . . . 3 . 
     `, SpriteKind.Enemy)
-Ghosty4.setPosition(randint(0, 10), randint(0, 10))
+Ghosty4.setBounceOnWall(true)
+Ghosty4.setPosition(280, 134)
+Ghosty4.setVelocity(38, 40)
 tiles.setCurrentTilemap(tilemap`level2`)
 controller.moveSprite(Player1, 100, 100)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . 
+    . . . . . . . . . . 7 . . 
+    . . . . . . 7 7 7 7 7 . . 
+    . 2 2 2 7 7 7 . . . 7 . . 
+    2 2 2 1 2 7 . . . 7 7 . . 
+    2 2 2 1 1 2 d 2 . 7 . . . 
+    2 2 2 1 2 d 2 2 2 7 . . . 
+    2 2 2 d d 2 2 2 2 2 . . . 
+    2 2 d 2 2 2 2 2 2 2 2 . . 
+    . d 2 2 2 2 2 2 2 1 2 . . 
+    . . d 2 2 2 2 2 2 2 2 . . 
+    . . . 2 2 2 2 2 2 2 . . . 
+    . . . . 2 2 2 2 2 . . . . 
+    `, SpriteKind.Food)
